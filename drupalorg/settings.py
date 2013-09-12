353d6caef845e6e32dad42d291ff6eb7f4a31906
php5engineer@gmail.com <php5engineer@gmail.com>
2013-09-12 14:59:26 +0400

    file structure refactorings

--- drupalorg/settings.py
+++ drupalorg/settings.py
@@ -1,3 +1,11 @@
+##################
+# parsley settings
+##################
+PARSLEY_CRAWLER_ROOT = "/home/ubuntu/Programs/drupal/scrapy-parsley/drupalorg"
+PARSLEY_FILES_ROOT = "/home/ubuntu/Programs/drupal/files"
+PARSLEY_PAGES_LIMIT = 5  # how many pages to crawl
+
+
 #################
 # scrapy settings
 #################
@@ -9,10 +17,10 @@ HTTPCACHE_ENABLED = True
 # HTTPCACHE_STORAGE = 'scrapy.contrib.downloadermiddleware.httpcache.FilesystemCacheStorage'  # old path
 HTTPCACHE_STORAGE = 'scrapy.contrib.httpcache.FilesystemCacheStorage'  # new path
 HTTPCACHE_EXPIRATION_SECS = 0
-HTTPCACHE_DIR = '/home/ubuntu/Programs/drupal/files/cached_pages'
+HTTPCACHE_DIR = PARSLEY_FILES_ROOT + '/cached_pages'
 
 IMAGES_MIN_HEIGHT = 110
-IMAGES_STORE = '/home/ubuntu/Programs/drupal/files/images'
+IMAGES_STORE = PARSLEY_FILES_ROOT + '/images'
 
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.contrib.downloadermiddleware.httpcache.HttpCacheMiddleware': 543,
@@ -27,17 +35,11 @@ ITEM_PIPELINES = [
 ]
 
 
-##################
-# parsley settings
-##################
-PARSLEY_CRAWLER_ROOT = "/home/ubuntu/Programs/drupal/scrapy-parsley/drupalorg"
-PARSLEY_PAGES_LIMIT = 5  # how many pages to crawl
-
-
 ###########################
 # project-specific settings
 ###########################
-PARSLEY_CONTRIBUTED_PROJECTS_ROOT = "/home/ubuntu/Programs/drupal/files/git"
+PARSLEY_CONTRIBUTED_PROJECTS_ROOT = PARSLEY_FILES_ROOT + '/git'
 PARSLEY_CORE_PROJECTS_ROOT = PARSLEY_CONTRIBUTED_PROJECTS_ROOT + '/drupal/modules'
 
 PARSLEY_SUPPORTED_MAJOR_VERSIONS = [6, 7]
+PARSLEY_GIT_TAGS_CSV_PATH = PARSLEY_FILES_ROOT + '/projects_releases.csv'
